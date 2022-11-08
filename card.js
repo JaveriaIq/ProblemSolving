ba07c0209bd761b5f8ec89d5894d880cc051017d
JaveriaIq <92935652+JaveriaIq@users.noreply.github.com>
2022-11-08 09:23:25 +0500

    Update card.js

--- card.js
+++ card.js
@@ -6,7 +6,7 @@ const num = prompt('Card number: ');                //user input
 let arr = num.split("");                            //string to array
 console.log(arr);
 let filtered = arr.filter((a, i) => i % 2 !== 1);       // odd indexes of an array
-console.log(filtered);
+//console.log(filtered);
 var unfiltered = filtered.map(function (element) {
     return element * 2;                               //multiply by 2
 });
@@ -14,9 +14,9 @@ console.log(unfiltered);
 var sum = unfiltered.reduce(function (x, y) {       // add together
     return x + y;
 }, 0);
-console.log(sum);
+//console.log(sum);
 const last = String(sum).slice(-1);                 //get last digit 
-console.log(last);
+//console.log(last);
 if (last == 0 && arr.length == 13 || last == 0 && arr.length == 15 || last == 0 && arr.length == 16) //accepts if cardno. length is 13,15,16 and last digit is 0
 {
     const ab = arr.slice(0, 2);                      //get starting two digits
